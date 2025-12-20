2c3382fdf053536d756efb493f8931661f820f41
Deydson Costa <103591633+falloutdey@users.noreply.github.com>
2025-12-19 21:14:33 -0300

    Organização de package e arquivos iniciais

---
+++ digital-banking-system/src/br/com/falloutdey/bank/application/App.java
@@ -0,0 +1,11 @@
+package br.com.falloutdey.bank.application;
+
+import br.com.falloutdey.bank.domain.model.User;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        
+        // User usuario = new User();
+    }
+}
---
+++ digital-banking-system/src/br/com/falloutdey/bank/domain/model/Conta.java
@@ -0,0 +1,27 @@
+package br.com.falloutdey.bank.domain.model;
+public class Conta {
+    private String tipoConta;
+    private double saldo;
+
+    public Conta(String tipoConta, double saldo) {
+        this.tipoConta = tipoConta;
+        this.saldo = saldo;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
